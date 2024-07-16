516ca1afa31fb26296a93e874b94205b7b0f287a
SwadheenMishra <144024958+SwadheenMishra@users.noreply.github.com>
2024-07-16 10:08:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import tkinter as tk
+from tkinter import scrolledtext
+from gtts import gTTS
+from playsound import playsound
 
+# Function to convert text to speech
+def speak_text(text: str) -> None:
+    tts = gTTS(text, lang='en')
+    tts.save("temp.mp3")
+    playsound("temp.mp3")
+    os.remove("temp.mp3")
+
+def main():
+    speak_text('test - Hack CLub') # its working!!
+
+if __name__ == '__main__':
+    main()
+        
