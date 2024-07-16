b2322e1f54a8085aec4533e273de2487d773a9b9
SwadheenMishra <144024958+SwadheenMishra@users.noreply.github.com>
2024-07-16 11:49:44 -0700

    Update main.py
    using eleven labs now instead of gtts

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import os
-from gtts import gTTS
-from playsound import playsound
 import speech_recognition as sr
 from datetime import datetime
+import random
+import elevenlabs
+import elevenlabs.client
+
+# ===this is just to hide my api key===
+import API
+
+APIKEY = API.get_api_key()
+# ====================================== 
+
 
 
 def speak(text: str) -> None:
-    tts = gTTS(text, tld='us', lang='en', slow=False)
-    tts.save("temp.mp3")
-    playsound("temp.mp3")
-    os.remove("temp.mp3")
+    global APIKEY
+
+    client = elevenlabs.client.ElevenLabs(api_key=APIKEY)
+    audio = client.generate(text=text,voice=elevenlabs.Voice(
+            voice_id='TX3LPaxmHKxFdv7VOQHJ',
+            settings=elevenlabs.VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
+        ))
+
+    elevenlabs.play(audio)
 
 def take_Command() -> str:
     r = sr.Recognizer()
@@ -30,23 +42,21 @@ def take_Command() -> str:
     return query
 
 def main():
-    speak('System Online!')
+    # resposeHello = ['Hi!']
+
+    speak('System Online.')
+    
     while True:
         query = take_Command().lower()
 
-        if 'good night' in query:
-            speak('Good Night Sir!')
-        elif 'introduce yourself' in query:
-            speak('I am an AI built to help with day to day tasks!')
+        if 'hello' in query:
+            speak('Hi! How may i assist you?')
         elif 'hai' in query:
-            speak('Hello! How may i assist you?')
-        elif 'hello' in query:
-            speak('Hi! how can i help you today?')
-        elif 'what time' or 'whats the time' or 'what time is it' in query:
-            now = datetime.now()
-            time = now.strftime("%H:%M:%S")
-            speak(f'The time is {time}')
+            speak('Hello! How can i help you?')
+        elif 'introduce yourself' in query:
+            speak('My name is david and i am an AI built to help with day to day tasks!')
+        
+        
 
 if __name__ == '__main__':
     main()
-        
